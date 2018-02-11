55423db0ab08ca029439fdb81b708464d5410cc2
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-11 16:54:26 +0100

    rewrite setup.py

--- setup.py
+++ setup.py
@@ -1,63 +1,51 @@
 # coding=utf-8
 
-from setuptools import setup, find_packages
 
-install_requires = [
-    'click',
-    'certifi',
-    'urllib3',
-    'requests',
-    'click_log',
-    'jinja2',
-    'pyyaml',
-    'setuptools-scm',
-    'everett',
-]
 
-test_requires = [
-    'pytest',
-    'pytest-pycharm',
-    'pytest-pep8',
-    'pytest-cache',
-    'pytest-catchlog',
-    'pytest-cov',
-    'pytest-mock',
-    'pytest-pep8',
-    'coverage',
-    'flake8',
-    'pylint',
-    'safety',
-    'hypothesis',
+from pip.req import parse_requirements
+from setuptools import find_packages, setup
 
-]
+requirements = [str(r.req) for r in
+                parse_requirements('requirements.txt', session=False)]
+test_requirements = [str(r.req) for r in
+                     parse_requirements('requirements-dev.txt', session=False)]
 
-dev_requires = [
-    'pip-tools',
-]
-
-setup_requires = [
-    'pytest-runner',
-    'setuptools_scm',
-]
+CLASSIFIERS = filter(None, map(str.strip,
+                               """
+Development Status :: 2 - Pre-Alpha
+Environment :: Console
+Environment :: Win32 (MS Windows)
+Intended Audience :: End Users/Desktop
+Natural Language :: English
+Operating System :: Microsoft :: Windows :: Windows 7
+Operating System :: Microsoft :: Windows :: Windows 8
+Operating System :: Microsoft :: Windows :: Windows 8.1
+Operating System :: Microsoft :: Windows :: Windows 10
+License :: OSI Approved :: MIT License
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Topic :: Documentation
+Topic :: Text Processing
+Topic :: Text Processing :: Markup
+Topic :: Text Processing :: Markup :: LaTeX
+Topic :: Utilities
+""".splitlines()))
 
 entry_points = '''
 [console_scripts]
 edlm=edlm.cli:cli
 '''
 
-
-def main():
-    setup(
-        name='edlm',
-        use_scm_version=True,
-        install_requires=install_requires,
-        entry_points=entry_points,
-        tests_require=test_requires,
-        setup_requires=setup_requires,
-        test_suite='pytest',
-        packages=find_packages('.'),
-    )
-
-
-if __name__ == '__main__':
-    main()
+setup(
+    name='edlm',
+    use_scm_version=True,
+    install_requires=requirements,
+    tests_require=test_requirements,
+    entry_points=entry_points,
+    setup_requires=['setuptools_scm'],
+    test_suite='pytest',
+    packages=find_packages('.'),
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=CLASSIFIERS,
+)
