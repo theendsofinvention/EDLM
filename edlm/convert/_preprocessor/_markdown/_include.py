b6121988de65df25b759067d09caa05f790036e3
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-18 02:12:22 +0100

    fix include issues

--- edlm/convert/_preprocessor/_markdown/_include.py
+++ edlm/convert/_preprocessor/_markdown/_include.py
@@ -10,30 +10,40 @@ import elib
 from . import Context
 
 
-def _process_local_include(ctx: Context, include: Path):
-    from . import process_markdown
-    include_str = f'//include "{include.relative_to(ctx.source_folder)}"'
-    sub_context = ctx.get_sub_context()
-    sub_context.markdown_text = Path(ctx.source_folder, include).read_text(encoding='utf8')
-    sub_context.index_file = include
-    sub_context.includes = []
-    process_markdown(sub_context)
-    ctx.markdown_text = ctx.markdown_text.replace(include_str, sub_context.markdown_text)
-    ctx.images_used.update(sub_context.images_used)
+class Inclusion:
+    sub_context: Context
+    include_str: str
+    markdown_source: Path
+    new_index: Path
 
 
-def _process_external_include(ctx: Context, include: Path):
+def _process_include(ctx: Context, inclusion: Inclusion):
     from . import process_markdown
     from ..._get_includes import get_includes
-    include_str = f'//include "{include.relative_to(ctx.source_folder.parent)}"'
-    sub_context = ctx.get_sub_context()
-    sub_context.source_folder = include
-    sub_context.markdown_text = Path(include, 'index.md').read_text(encoding='utf8')
-    sub_context.index_file = include
-    sub_context.includes = []
-    get_includes(sub_context)
-    process_markdown(sub_context)
-    ctx.markdown_text = ctx.markdown_text.replace(include_str, sub_context.markdown_text)
+    inclusion.sub_context.markdown_text = inclusion.markdown_source.read_text(encoding='utf8')
+    inclusion.sub_context.index_file = inclusion.new_index
+    inclusion.sub_context.includes = []
+    get_includes(inclusion.sub_context)
+    process_markdown(inclusion.sub_context)
+    ctx.markdown_text = ctx.markdown_text.replace(inclusion.include_str, inclusion.sub_context.markdown_text)
+    if inclusion.sub_context.latex_refs:
+        refs = set(inclusion.sub_context.latex_refs)
+        refs.update(ctx.latex_refs)
+        ctx.latex_refs = list(refs)
+
+
+def _process_local_include(ctx: Context, inclusion: Inclusion):
+    inclusion.include_str = f'//include "{inclusion.new_index.relative_to(ctx.source_folder)}"'
+    inclusion.markdown_source = Path(ctx.source_folder, inclusion.new_index)
+    ctx.images_used.update(inclusion.sub_context.images_used)
+    _process_include(ctx, inclusion)
+
+
+def _process_external_include(ctx: Context, inclusion: Inclusion):
+    inclusion.include_str = f'//include "{inclusion.new_index.relative_to(ctx.source_folder.parent)}"'
+    inclusion.markdown_source = Path(inclusion.new_index, 'index.md')
+    inclusion.sub_context.source_folder = inclusion.new_index
+    _process_include(ctx, inclusion)
 
 
 def _get_unprocessed_includes(ctx: Context):
@@ -51,10 +61,13 @@ def process_includes(ctx: Context):
     """
     ctx.unprocessed_includes = []
     for include in ctx.includes:
+        inclusion = Inclusion()
+        inclusion.sub_context = ctx.get_sub_context()
+        inclusion.new_index = include
         if include.is_file():
-            _process_local_include(ctx, include)
+            _process_local_include(ctx, inclusion)
         elif include.is_dir():
-            _process_external_include(ctx, include)
+            _process_external_include(ctx, inclusion)
 
     _get_unprocessed_includes(ctx)
     if ctx.unprocessed_includes:
