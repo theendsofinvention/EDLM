cdd4c7b4b1aa0e24b3d0bcc6e145710bde7f0e22
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-18 02:24:43 +0100

    fix external includes not finding media folders

--- edlm/convert/_preprocessor/_markdown/_include.py
+++ edlm/convert/_preprocessor/_markdown/_include.py
@@ -14,16 +14,18 @@ class Inclusion:
     sub_context: Context
     include_str: str
     markdown_source: Path
-    new_index: Path
+    parent_folder: Path
 
 
 def _process_include(ctx: Context, inclusion: Inclusion):
     from . import process_markdown
     from ..._get_includes import get_includes
+    from ..._get_media_folders import get_media_folders
     inclusion.sub_context.markdown_text = inclusion.markdown_source.read_text(encoding='utf8')
-    inclusion.sub_context.index_file = inclusion.new_index
+    inclusion.sub_context.index_file = inclusion.markdown_source
     inclusion.sub_context.includes = []
     get_includes(inclusion.sub_context)
+    get_media_folders(inclusion.sub_context)
     process_markdown(inclusion.sub_context)
     ctx.markdown_text = ctx.markdown_text.replace(inclusion.include_str, inclusion.sub_context.markdown_text)
     if inclusion.sub_context.latex_refs:
@@ -33,16 +35,16 @@ def _process_include(ctx: Context, inclusion: Inclusion):
 
 
 def _process_local_include(ctx: Context, inclusion: Inclusion):
-    inclusion.include_str = f'//include "{inclusion.new_index.relative_to(ctx.source_folder)}"'
-    inclusion.markdown_source = Path(ctx.source_folder, inclusion.new_index)
-    ctx.images_used.update(inclusion.sub_context.images_used)
+    inclusion.include_str = f'//include "{inclusion.parent_folder.relative_to(ctx.source_folder)}"'
+    inclusion.markdown_source = Path(ctx.source_folder, inclusion.parent_folder)
     _process_include(ctx, inclusion)
+    ctx.images_used.update(inclusion.sub_context.images_used)
 
 
 def _process_external_include(ctx: Context, inclusion: Inclusion):
-    inclusion.include_str = f'//include "{inclusion.new_index.relative_to(ctx.source_folder.parent)}"'
-    inclusion.markdown_source = Path(inclusion.new_index, 'index.md')
-    inclusion.sub_context.source_folder = inclusion.new_index
+    inclusion.include_str = f'//include "{inclusion.parent_folder.relative_to(ctx.source_folder.parent)}"'
+    inclusion.markdown_source = Path(inclusion.parent_folder, 'index.md')
+    inclusion.sub_context.source_folder = inclusion.parent_folder
     _process_include(ctx, inclusion)
 
 
@@ -63,7 +65,7 @@ def process_includes(ctx: Context):
     for include in ctx.includes:
         inclusion = Inclusion()
         inclusion.sub_context = ctx.get_sub_context()
-        inclusion.new_index = include
+        inclusion.parent_folder = include
         if include.is_file():
             _process_local_include(ctx, inclusion)
         elif include.is_dir():
