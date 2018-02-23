0fad6b6c67454497c2af7f5f4634415a0696e8a4
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-23 14:43:27 +0100

    linting

--- test/test_convert/test_template.py
+++ test/test_convert/test_template.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 import pytest
 
-from edlm.convert import Context, _get_template as template
+from edlm.convert import Context
+from edlm.convert import _get_template as template
 
 
 def test_no_template():
